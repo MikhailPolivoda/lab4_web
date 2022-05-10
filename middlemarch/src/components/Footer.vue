727174640d641f07f336b0a28c09428fb5562f11
MikhailPolivoda <mr.poliwoda2016@yandex.ru>
2022-05-10 18:35:08 +0300

    1

---
+++ middlemarch/src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+            <div class="social-links">
+
+            </div>
+
+            <p>Выполнили студенты группы АВТ-913 Малахов Д. и Поливода М..</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 40px;
+  text-align: center;
+  background-color: #111222;
+  padding-top: 25px;
+  font-size: 16px;
+  font-weight: 200;
+  color: #969696;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
